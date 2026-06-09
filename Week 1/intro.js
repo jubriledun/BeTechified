63c7b14f3116718e6b9e7f6ecdc9aacca2284bcd
Jubril Edun <jubriledun@gmail.com>
2026-06-09 21:30:12 +0100

    changed console log message

--- Week 1/intro.js
+++ Week 1/intro.js
@@ -2,4 +2,4 @@ let name = "Jubril Edun";
 let age = 28;
 let favProgLang = "NodeJS";
 
-console.log(name + "'s age is " + age + " years old and his favourite programming language is " + favProgLang);
+console.log(name + "\n" + age + "\n" + favProgLang);
