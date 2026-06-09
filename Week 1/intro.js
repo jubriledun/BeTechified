f9b033e9c8423f14b019a8e638a93c638f67eafa
Jubril Edun <jubriledun@Zest-005-Macbook.local>
2026-06-09 21:12:33 +0100

    added intro.js

---
+++ Week 1/intro.js
@@ -0,0 +1,5 @@
+let name = "Jubril Edun";
+let age = 28;
+let favProgLang = "NodeJS";
+
+console.log(name + "'s age is " + age + " years old and his favourite programming language is " + favProgLang);
